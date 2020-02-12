d58591c2e36888ee4ed688c1673f2fabcca76505
sefrem <fireweb2112@gmail.com>
2020-02-12 22:36:44 +0300

    upload files with react-dropzone

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -3,83 +3,78 @@ import { useDropzone } from "react-dropzone";
 const store = require("store");
 
 function Dash(props) {
-	const [files, setFiles] = useState([]);
-	const [error, setError] = useState([]);
+  const [files, setFiles] = useState([]);
+  const [error, setError] = useState([]);
 
-	const { getRootProps, getInputProps } = useDropzone({
-		accept: "image/*",
-		onDrop: acceptedFiles => {
-	    setError('');
-	    if(files.length + acceptedFiles.length > 5) {
-	      return setError("Не больше 5 файлов")
-	    }
-      acceptedFiles.map(file => console.log(file))
-	// const images = [];
-	// acceptedFiles.forEach(file => {
-	//   let url = file.path;
-	//   let preview = URL.createObjectURL(file);
-	//   let image = {url: url, preview: preview}
-	//   images.push(image)
-	// })
-	// store.set('images', JSON.stringify(images))
 
-			setFiles(
-				files.concat(
-					acceptedFiles.map(file =>
-						Object.assign(file, {
-							preview: URL.createObjectURL(file),
-						})
-					)
-				)
-	    );
 
-	    console.log(files);
-		},
-	});
+    const { getRootProps, getInputProps } = useDropzone({
+      accept: "image/*",
+      onDrop: acceptedFiles => {
+        setError("");
+        if (files.length + acceptedFiles.length > 5) {
+          return setError("Не больше 5 файлов");
+        }
+		acceptedFiles.map(file => console.log(file));
+		
+        // const images = [];
+        // acceptedFiles.forEach(file => {
+        //   let url = file.path;
+        //   let preview = URL.createObjectURL(file);
+        //   let image = {url: url, preview: preview}
+        //   images.push(image)
+        // })
+        // store.set('images', JSON.stringify(images))
 
-	//JSON { images: [{url: ‘’, preview: ‘’}] }
+        setFiles(
+          files.concat(
+            acceptedFiles.map(file =>
+              Object.assign(file, {
+                preview: URL.createObjectURL(file)
+              })
+            )
+          )
+        );
 
-	const thumbs = files.map(file => (
-		<div className="preview" key={file.name}>
-			<div className="preview__thumb">
-				<img src={file.preview} alt="" className="preview__img" />
-			</div>
-		</div>
-	));
+        console.log(files);
+      }
+    });
 
-	useEffect(
-		() => () => {
-			// Make sure to revoke the data uris to avoid memory leaks
-			files.forEach(file => URL.revokeObjectURL(file.preview));
-		},
-		[files]
-	);
+    //JSON { images: [{url: ‘’, preview: ‘’}] }
 
-  // https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
-
-	return (
-		<div className="dashboard">
-			<div className="previews">
-				{thumbs}
-				<div {...getRootProps({ className: "file-selection" })}>
-					<input {...getInputProps()} />
-				{/* <div className="drop-area">
-					<input
-						type="file"
-						id="fileElem"
-						className="file-selection"
-						multiple
-						accept="image/*"
-						// onChange={handleFiles(this.files)}
-					/>
-				</div> */}
-				<div>{error}</div>
-			</div>
+    const thumbs = files.map(file => (
+      <div className="preview" key={file.name}>
+        <div className="preview__thumb">
+          <img src={file.preview} alt="" className="preview__img" />
+        </div>
       </div>
-		</div>
-	);
+    ));
+
+    useEffect(
+      () => () => {
+        // Make sure to revoke the data uris to avoid memory leaks
+        files.forEach(file => URL.revokeObjectURL(file.preview));
+      },
+      [files]
+    );
+
+//   https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+
+
+  return (
+    <div className="dashboard">
+      <div className="previews">
+        {thumbs} 
+         <div {...getRootProps({ className: "file-selection" })}>
+          <input {...getInputProps()} /> 
+        </div>
+		<div>{error}</div>
+      </div>
+	</div>
+
+
+  );
 }
 
 export default Dash;
-
 
