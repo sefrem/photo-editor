9eb1c6ac4e127679627aeff034389d048983c157
sefrem <fireweb2112@gmail.com>
2020-02-14 22:03:05 +0300

    still working on editor customization

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -3,7 +3,7 @@ import { useDrop } from 'react-dnd'
 import Dropzone from './Dropzone'
 import Preview from './Preview'
 import update from 'immutability-helper'
-import Editor from "./Editor"
+import Editor from './Editor'
 
 const Dash = () => {
   const [files, setFiles] = useState([])
@@ -36,28 +36,28 @@ const Dash = () => {
   const [, drop] = useDrop({ accept: 'preview' })
   return (
     <>
-    <div className="dashboard">
-      <div className="previews" ref={drop}>
-        {files.map((file, index) => (
-          <Preview
-            id={index}
-            key={file.name}
-            file={file}
-            index={index}
-            files={files}
-            setFiles={setFiles}
-            moveFile={moveFile}
-            findFile={findFile}
-            onClick={onClick}
-          />
-        ))}
+      <div className="dashboard">
+        <div className="previews" ref={drop}>
+          {files.map((file, index) => (
+            <Preview
+              id={index}
+              key={file.name}
+              file={file}
+              index={index}
+              files={files}
+              setFiles={setFiles}
+              moveFile={moveFile}
+              findFile={findFile}
+              onClick={onClick}
+            />
+          ))}
 
-        <Dropzone files={files} setFiles={setFiles} setError={setError} />
+          <Dropzone files={files} setFiles={setFiles} setError={setError} />
+        </div>
+        <div>{error}</div>
       </div>
-      <div>{error}</div>
-    </div>
-    <div id="tui-image-editor"></div>
-    {selectedId ? <Editor files={files} id={selectedId}/> : null}
+      <div id="tui-image-editor"></div>
+      {selectedId ? <Editor files={files} id={selectedId} /> : null}
     </>
   )
 }
--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,76 +1,40 @@
 import React from 'react'
 import 'tui-image-editor/dist/tui-image-editor.css'
 // import ImageEditor from '@toast-ui/react-image-editor'
-var ImageEditor = require('tui-image-editor');
+var ImageEditor = require('tui-image-editor')
 
 const Editor = props => {
-  // imageEditor.addImageObject('path/fileName.jpg').then(objectProps => {
-  //     console.log(ojectProps.id);
-  // });
   const { files, id } = props
   const path = files[id].preview
   const name = files[id].name
 
+  var imageEditor = new ImageEditor(
+    document.querySelector('#tui-image-editor'),
+    {
+      includeUI: {
+        loadImage: {
+          path: `${path}`,
+          name: `${name}`,
+        },
+      },
+      menu: ['crop', 'rotate', 'draw'],
+      initMenu: 'crop',
+      uiSize: {
+        width: '375px',
+        height: '810px',
+      },
+      menuBarPosition: 'bottom',
 
-var imageEditor = new ImageEditor(document.querySelector('#tui-image-editor'), {
-//   includeUI: {
-//     loadImage: {
-//       path: `${path}`,
-//       name: `${name}`
-//     }},
-//     menu: ['crop', 'rotate', 'draw'],
-//     undo: false,
-//     initMenu: 'crop',
-//     uiSize: {
-//         width: '375px',
-//         height: '810px'
-//     },
-//     // menuBarPosition: 'bottom'
-//   },
-  cssMaxWidth: 375,
-  cssMaxHeight: 500,
-  selectionStyle: {
-    cornerSize: 20,
-    rotatingPointOffset: 70
-  }
-});
-// let image;
-// let str = path.substring(path.indexOf('h'))
-// let file = new File(str, name)
-
-// imageEditor.loadImageFromURL(path, 'FilterImage');
-
-
-// instance.crop(instance.getCropzoneRect())
-
-  return (
-    <>
-      {/* <ImageEditor
-        includeUI={{
-          loadImage: {
-            path: `${path}`,
-            name: `${name}`,
-          },
-          menu: ['crop', 'rotate', 'draw'],
-          initMenu: 'crop',
-          uiSize: {
-            width: '375px',
-            height: '810px',
-          },
-          menuBarPosition: 'bottom',
-        }}
-        cssMaxHeight={420}
-        cssMaxWidth={335}
-        selectionStyle={{
-          cornerSize: 20,
-          rotatingPointOffset: 70,
-        }}
-      /> */}
-      {/* {image} */}
-      <button>Skip</button>
-      <button>Save</button>
-    </>
+      cssMaxHeight: 420,
+      cssMaxWidth: 335,
+      selectionStyle: {
+        cornerSize: 20,
+        rotatingPointOffset: 70,
+      },
+    }
   )
+
+  return <></>
 }
 
 export default Editor
