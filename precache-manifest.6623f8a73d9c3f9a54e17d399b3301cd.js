0cfca38f67d28713673af7b45e53f9b3810732d1
sefrem <fireweb2112@gmail.com>
2020-02-20 22:49:14 +0300

    Updates

---
+++ precache-manifest.6623f8a73d9c3f9a54e17d399b3301cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ce1cb83c05afb775c840f3d2949a99",
+    "url": "/photo-editor/index.html"
+  },
+  {
+    "revision": "e7ee388f853d3b9a7a51",
+    "url": "/photo-editor/static/css/2.49fc67f7.chunk.css"
+  },
+  {
+    "revision": "891f0f76460423617f64",
+    "url": "/photo-editor/static/css/main.dcd56b27.chunk.css"
+  },
+  {
+    "revision": "e7ee388f853d3b9a7a51",
+    "url": "/photo-editor/static/js/2.29599870.chunk.js"
+  },
+  {
+    "revision": "4a646b7f6ff9e5c00df182bd99eca67b",
+    "url": "/photo-editor/static/js/2.29599870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891f0f76460423617f64",
+    "url": "/photo-editor/static/js/main.3aa25cae.chunk.js"
+  },
+  {
+    "revision": "504c4cce1eb373ec5e27",
+    "url": "/photo-editor/static/js/runtime-main.430e69ba.js"
+  },
+  {
+    "revision": "843c03ed6edd3f2a4dc35357feedbbec",
+    "url": "/photo-editor/static/media/icon-a.843c03ed.svg"
+  },
+  {
+    "revision": "d70663d06bd44437c178f522047470cc",
+    "url": "/photo-editor/static/media/icon-b.d70663d0.svg"
+  },
+  {
+    "revision": "6f440c0d2fa852de523edfd5383ad682",
+    "url": "/photo-editor/static/media/icon-c.6f440c0d.svg"
+  },
+  {
+    "revision": "4ef3abbd14fbe85ceebf36d900d2c9ad",
+    "url": "/photo-editor/static/media/icon-d.4ef3abbd.svg"
+  }
+]);
